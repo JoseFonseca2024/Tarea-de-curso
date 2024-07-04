c6d935655a0969ceeacad6366a5e5516719fdde4
JoseFonseca2024 <jose.fonseca641@std.uni.edu.ni>
2024-07-03 20:25:52 -0600

    Parte estructural del codigo

--- src/App.java
+++ src/App.java
@@ -12,9 +12,9 @@ public class App {
         boolean continuar = true;
 
         do {
-            
-            System.out.println("Sistema de operaciones de caja");
-
+            System.out.println("==================================");
+            System.out.println("||Sistema de operaciones de caja||");
+            System.out.println("==================================");
             System.out.println("Seleccione una opción:");
 
                 System.out.println("1. Ingresar saldo");
@@ -27,22 +27,27 @@ public class App {
 
             switch (respuesta) {
                 case 1:
+                System.out.println("==================================");
                     ingresarsaldo(leer);
                     break;
                 
                 case 2:
+                System.out.println("==================================");
                     registrarEntrada(leer);
                     break;
                 
                 case 3:
-
+                System.out.println("==================================");
+                    registrarSalida(leer);
                     break;
 
                 case 4: 
-
+                System.out.println("==================================");
+                    mostrarMovimientos();
                     break;
 
                 case 5:
+                System.out.println("==================================");
                     continuar = false;
                     break;
                 default:
@@ -81,5 +86,32 @@ public class App {
         movimientos.add(entrada);
         caja.registrarMovimiento(entrada);
         System.out.println("Entrada registrada correctamente.");
+    }
+
+    private static void registrarSalida(Scanner leer) {
+        System.out.println("Ingrese el monto de la salida: ");
+        double montoSalida = leer.nextDouble();
+        leer.nextLine(); 
+        if (montoSalida > caja.getSaldo()) {
+            System.out.println("Error: No se puede registrar una salida mayor al saldo disponible.");
+            return;
+        }
+        System.out.println("Ingrese la descripción de la salida: ");
+        String descripcionSalida = leer.nextLine();
+        System.out.println("Ingrese la fecha del movimiento (dia/mes/año): ");
+        String fecha = leer.nextLine();
+        Movimientos salida = new Salidas(montoSalida, descripcionSalida, fecha);
+        movimientos.add(salida);
+        caja.registrarMovimiento(salida);
+        System.out.println("Salida registrada correctamente.");
+    }
+    
+    private static void mostrarMovimientos() {
+       if (!movimientos.isEmpty()) {
+        for (Movimientos movimiento : movimientos) {
+            System.out.println(movimiento.getFecha() + " - " + movimiento.getTipo() + " - " + movimiento.getMonto() + " | " +movimiento.getDescripcion()+ " | ");
+        }
+       }
+        System.out.println("Saldo actual de caja: "+caja.getSaldo());
     }
 }
