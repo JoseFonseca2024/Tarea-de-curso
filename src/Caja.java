fe893c1c0ff3a3e4a9160799b9930044e3eed987
JoseFonseca2024 <jose.fonseca641@std.uni.edu.ni>
2024-07-03 18:32:43 -0600

    Actualizacion en clase Caja

--- src/Caja.java
+++ src/Caja.java
@@ -1,3 +1,13 @@
 public class Caja {
 
+    private double saldo;
+
+    
+    public Caja (double saldo) {
+        this.saldo = saldo = 0.00;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 }
