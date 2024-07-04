de1d08e47a1f0e8c2d68513dc147c6791d97a263
JoseFonseca2024 <jose.fonseca641@std.uni.edu.ni>
2024-07-03 19:55:21 -0600

    Agregando metodo ingresarsaldo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,52 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
+
+    private static ArrayList<Movimientos> movimientos = new ArrayList<>();
+    private static Caja caja = new Caja(0);;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner leer = new Scanner(System.in);
+
+        boolean continuar = true;
+
+        do {
+            
+            System.out.println("Sistema de operaciones de caja");
+
+            System.out.println("Seleccione una opción:");
+
+                System.out.println("1. Ingresar saldo");
+                System.out.println("2. Registrar entrada");
+                System.out.println("3. Registrar salida");
+                System.out.println("4. Mostrar movimientos y saldo actual");
+                System.out.println("5. Salir");
+
+            int respuesta = leer.nextInt();
+
+            switch (respuesta) {
+                case 1:
+                    ingresarsaldo(leer);
+                    break;
+            
+                default:
+                    break;
+            }
+        } while (continuar);
+
+    }
+
+    private static void ingresarsaldo (Scanner leer) {
+        System.out.println("Ingrese el saldo existente en caja");
+        double valor = leer.nextDouble();
+        leer.nextLine();
+        System.out.println("Ingrese la fecha del movimiento (Formato dia/mes/año) ");
+        String fecha = leer.nextLine();
+        Movimientos entradaSaldo = new Entradas(valor, "Ingreso de saldo", fecha);
+        movimientos.add(entradaSaldo);
+        caja.registrarMovimiento(entradaSaldo);
+        System.out.println("Saldo registrado");
+
     }
 }
